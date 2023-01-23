6fa864ab4c640f9d32b0135d21d6e16c1eb3569f
Sean Ryan <sean.ryan@mendix.com>
2023-01-23 14:25:03 +0100

    Improve time support - do not assume data is sorted by time

--- README.md
+++ README.md
@@ -52,18 +52,18 @@ The images for the examples are at https://github.com/JasonObeid/Chart2TextImage
 | ![2.png](./examples/images/2.png) | Instagram had highest number of followers in millions 334.72. |
 | ![3.png](./examples/images/3.png) | Pittsburgh Steelers had highest super bowl wins 6. |
 | ![4.png](./examples/images/4.png) | Apple had highest market value in billion u.s. dollars 961.3. |
-| ![5.png](./examples/images/5.png) | - |
+| ![5.png](./examples/images/5.png) | League of Legends had highest share of playing time  22.92%. |
 
 Time-based charts:
 
 
 | Chart | Caption (auto-generated) |
 |---|---|
-| ![0.time.png](./examples/images/0.time.png) | Q4 '19 had highest number of users in millions 2498. Overall trend is decreasing. |
-| ![101.time.png](./examples/images/101.time.png) | 2024 has 2.54% inflation rate compared to previous year. 2023 has 2.47% inflation rate compared to previous year. 2022 has 2.33% inflation rate compared to previous year. 2021 has 1.97% inflation rate compared to previous year. 2020 has 1.82% inflation rate compared to previous year. 2019 has 1.6% inflation rate compared to previous year. |
-| ![104.time.png](./examples/images/104.time.png) | 2019.0 had highest billion u.s. dollars 514.41. Overall trend is decreasing. |
-| ![105.time.png](./examples/images/105.time.png) | 2018/19 had highest vehicle sales in million units 21.18. |
-| ![115.time.multiple-peaks.png](./examples/images/115.time.multiple-peaks.png) | - |
+| ![0.time.png](./examples/images/0.time.png) | Q1 '10 to Q1 '09 has an overall increasing trend. Q4 '19 had highest number of users in millions 2498. |
+| ![101.time.png](./examples/images/101.time.png) | 2024, 2023, 2022, 2021, 2020, 2019 had an average 2.12% inflation rate compared to previous year. 1984 to 2024* has an overall decreasing trend. 1997 had lowest inflation rate compared to previous year 0.23%. |
+| ![104.time.png](./examples/images/104.time.png) | 2006 to 2019 has an overall increasing trend. 2019 had highest billion u.s. dollars 514.41. |
+| ![105.time.png](./examples/images/105.time.png) | 2010/11 to 2018/19 has an overall increasing trend. 2018/19 had highest vehicle sales in million units 21.18. |
+| ![115.time.multiple-peaks.png](./examples/images/115.time.multiple-peaks.png) | No overall trend. '10 had highest unemployment rate 9.6%. |
 
 #### More images
 The full set of images can be downloaded with the script:
@@ -77,3 +77,10 @@ Generate captions for the examples:
 ```
 ./test.sh
 ```
+
+# References
+
+| Topic | Links |
+|---|---|
+| Trend detection | https://stackoverflow.com/questions/42920537/finding-increasing-trend-in-pandas - https://abhinaya-sridhar-rajaram.medium.com/mann-kendall-test-in-python-for-trend-detection-in-time-series-bfca5b55b |
+| Time series support | https://ourcodingclub.github.io/tutorials/pandas-time-series/ — https://towardsdatascience.com/analyzing-time-series-data-in-pandas-be3887fdd621?gi=fabb1396ed42 |
--- src/generate_summary.py
+++ src/generate_summary.py
@@ -108,6 +108,65 @@ def format_value(value, is_percent):
         return f"{value}%"
     return value
 
+# show min and previous 2
+def summarize_decreasing_ts(df, name_column, value_column, is_percent):
+    summary = []
+    # TODO xxx support joint-min rows
+    # TODO xxx show next 2
+    min_row_value = df[value_column].min()
+    min_row_name = df[name_column][df[value_column].idxmin()]
+
+    summary.append(f"{clean_name(min_row_name)} had lowest {make_lower(value_column)} {format_value(min_row_value, is_percent)}")
+    return summary
+
+# show max and following 2
+def summarize_increasing_ts(df, name_column, value_column, is_percent):
+    summary = []
+    # TODO xxx support joint-max rows
+    # TODO xxx show next 2
+    max_row_value = df[value_column].max()
+    max_row_name = df[name_column][df[value_column].idxmax()]
+
+    summary.append(f"{clean_name(max_row_name)} had highest {make_lower(value_column)} {format_value(max_row_value, is_percent)}")
+    return summary
+
+# show max and following 2
+def summarize_non_ts(df, name_column, value_column, is_percent):
+    summary = []
+    # TODO xxx support joint-max rows
+    max_row_value = df[value_column].max()
+    max_row_name = df[name_column][df[value_column].idxmax()]
+
+    summary.append(f"{clean_name(max_row_name)} had highest {make_lower(value_column)} {format_value(max_row_value, is_percent)}")
+    return summary
+
+def convert_to_sortable_time(time_value):
+    if is_string(time_value):
+        quarters = ["Q1", "Q2", "Q3", "Q4"]
+        for quarter in quarters:
+            if time_value.startswith(quarter):
+                remaining_parts = time_value.split(' ')[1:]
+                remaining_parts_as_time = []
+                for part in remaining_parts:
+                    remaining_parts_as_time.append(convert_to_sortable_time(part))
+                return ' '.join(remaining_parts_as_time)
+        # like "2018/19"
+        if re.search("^\d{4}/\d+$", time_value):
+            return time_value
+        # like "'10"
+        if re.search("^'\d{2}$", time_value):
+            year_2_digit = int(time_value[1:])
+            if year_2_digit >= 70:
+                return f"19{year_2_digit}"
+            return f"20{year_2_digit}"
+        # like "2024*" or "2024"
+        if re.search("^\d{4}(\*)?$", time_value):
+            return time_value.replace('*', '')
+    return time_value
+
+def add_time_column(df, name_column, time_column):
+    df[time_column] = df.apply(lambda row: convert_to_sortable_time(row[name_column]), axis=1)
+
 def summarize(df):
     summary = []
 
@@ -162,36 +221,46 @@ def summarize(df):
                 summary.append(f"{clean_name(row[name_column])} has {format_value(row[value_column], is_percent)} {make_lower(value_column)}")
                 interesting_names.append(row[name_column])
 
-    # max
-    # TODO xxx support joint-max rows
-    if are_values_all_numeric(df, value_column):
-        max_row_value = df[value_column].max()
-        max_row_name = df[name_column][df[value_column].idxmax()]
-
-        if (max_row_name not in interesting_names):
-            summary.append(f"{clean_name(max_row_name)} had highest {make_lower(value_column)} {format_value(max_row_value, is_percent)}")
-    else:
-        # TODO xxx - fallback
-        summary.append("-")
-
-    # top 3
-    # xxx
-
-    # bottom 3
-    # xxx
-
-    # average
-    # xxx
+    is_all_numeric = are_values_all_numeric(df, value_column)
 
     # Time-based charts
-    if is_timeseries(df, name_column) and are_values_all_numeric(df, value_column):
-        df_values = df[value_column]
-        trend_test = mk.original_test(df_values, alpha=0.05)
-        trend_desc = trend_test.trend
-        if trend_desc == 'no trend':
-            summary.append(f"No overall trend")
-        else:
-            summary.append(f"Overall trend is {trend_test.trend}")
+    is_increasing = False
+    is_decreasing = False
+    if is_all_numeric:
+        if is_timeseries(df, name_column):
+            # need to sort by time, increasing
+            time_column = 'time_'
+            df_tmp = df.copy()
+            add_time_column(df_tmp, name_column, time_column)
+            df_tmp = df_tmp.sort_values(by=time_column, ascending=True)
+
+            df_values = df_tmp[value_column].values
+            trend_test = mk.original_test(df_values, alpha=0.05)
+            trend_desc = trend_test.trend
+            if trend_desc == 'no trend':
+                summary.append(f"No overall trend")
+            else:
+                first_period = df_tmp[name_column].values[0]
+                last_period = df_tmp[name_column].values[len(df)-1]
+                summary.append(f"{first_period} to {last_period} has an overall {trend_test.trend} trend")
+                if trend_test.trend == 'increasing':
+                    is_increasing = True
+                else:
+                    if trend_test.trend == 'decreasing':
+                        is_decreasing = True
+            # trend + decreasing -> show min and previous 2
+            if is_decreasing:
+                summary = summary + summarize_decreasing_ts(df_tmp, name_column, value_column, is_percent)
+            # no trend OR trend + increasing -> show max and following 2
+            else:
+                summary = summary + summarize_increasing_ts(df_tmp, name_column, value_column, is_percent)
+
+        else: # not timeseries
+            # -> show max and following 2
+            summary = summary + summarize_non_ts(df, name_column, value_column, is_percent)
+    else:
+        # fallback
+        summary.append("-")
 
     summary.append("")
 
